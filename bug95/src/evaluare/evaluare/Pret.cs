7e79627857c5a943eaa8ecd4ff1884e0fe6d9a18
Butnaru Gheorghita <32176636+BUG95@users.noreply.github.com>
2020-03-05 19:54:25 +0200

    Evaluare 1

---
+++ bug95/src/evaluare/evaluare/Pret.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evaluare
+{
+    class Pret
+    {
+        public decimal DaPret(Camera camera, byte numar_nopti)
+        {
+            bool reducere = false;
+
+            if (camera.Nume == "1")
+            {
+                return 200m * numar_nopti;
+
+            }
+            else if (camera.Nume == "2" || camera.Nume == "3")
+            {
+                return 120m * numar_nopti;
+            }
+
+            else if (camera.Nume == "4")
+            {
+                return 150m * numar_nopti;
+            }
+            else if (camera.Nume == "5" || camera.Nume == "6")
+            {
+                if (numar_nopti > 3) reducere = true;
+                if (reducere)
+                {
+
+                    decimal discount = 0.2m * (numar_nopti - 3);
+                    return 100m * numar_nopti - discount;
+
+                }
+                else return 100m * numar_nopti;
+            }
+            else throw new Exception("Camera inexistenta");
+        }
+
+    }
+}
