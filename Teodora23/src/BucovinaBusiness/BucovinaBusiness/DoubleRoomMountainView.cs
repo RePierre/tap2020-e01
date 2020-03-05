e4f9ca75a2cb00d0e40a4a64d895055559a5dd9f
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-03-05 18:49:56 +0200

    Structure of the project

---
+++ Teodora23/src/BucovinaBusiness/BucovinaBusiness/DoubleRoomMountainView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucovinaBusiness
+{
+    class DoubleRoomMountainView : Room
+    {
+
+    }
+}
---
+++ Teodora23/src/BucovinaBusiness/BucovinaBusiness/Room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucovinaBusiness
+{
+    public abstract class Room
+    {
+
+    }
+}
