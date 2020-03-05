42a2d95f403f6742a830371c487e1199baaf4bb2
anamariaginger <anamariacojocea98@gmail.com>
2020-03-05 19:56:58 +0200

    Cod

---
+++ anamariaginger/src/Family.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace anamariaginger
+{
+    class Family: Rooms
+    {
+        public Family(int roomnr, int nrnights) : base(roomnr, nrnights)
+        {
+
+        }
+       
+        public override decimal get_price_to_pay()
+        {
+            
+            if (roomnr == 1)
+                price = 200m*nrnights;
+            else if (nrnights > 3)
+                price = 200m *3 + 200m * (80 / 100) * (nrnights - 3);
+            return price;
+        }
+    }
+}
---
+++ anamariaginger/src/Single.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace anamariaginger
+{
+    class Single: Rooms
+    {
+        public Single(int roomnr, int nrnights) : base(roomnr, nrnights)
+        {
+
+        }
+        public override decimal get_price_to_pay()
+        {
+            if (roomnr == 5||roomnr==6)
+                price = 100m*nrnights;
+           else if (nrnights > 3)
+                price = 100m * 3 + 100m*(80/100)* (nrnights - 3);
+            return price;
+        }
+    }
+}
