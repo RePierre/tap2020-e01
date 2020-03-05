945a88b3e3f29e29fde67b49f22bc4f250841c65
DragosAlexa <dragos1899@gmail.com>
2020-03-05 19:05:26 +0200

    Update

---
+++ Evaluare1/Evaluare1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Evaluare1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bucovina Guest House!");
+            Console.WriteLine("Select which type of room do you prefer to stay?(Family room/Double Room/Double Room Mountain View/Attic Room)");
+            Console.ReadLine();
+        }
+    }
+}
