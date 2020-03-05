fcacb4b15714fde6dc621b81c0fc3a0aac05f948
GinigaGabriel <61537717+GinigaGabriel@users.noreply.github.com>
2020-03-05 19:55:13 +0200

    Add files via upload

---
+++ tap2020-e01-master/ginigagabriel/src/MicroApp/Reception.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace MicroApp
+{
+     class Reception
+    {
+
+        private static Reception write;
+
+        private Reception() { }
+
+        public static Reception WRITE
+        {
+            get
+            {
+                if (write == null)
+                {
+                    write = new Reception();
+                }
+                return write;
+            }
+        }
+
+        public void Writer(int nr, DateTime initial, DateTime final)
+        {
+            Console.WriteLine(Convert.ToString(Rooms.AUX.showPrice(nr, initial, final)));
+        }
+    }
+}
