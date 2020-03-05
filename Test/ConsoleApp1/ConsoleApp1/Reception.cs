31c3b7246a59ddf8674bcc52c0d90ae3ba934e1d
GalbinitaSebastian97 <61518348+GalbinitaSebastian97@users.noreply.github.com>
2020-03-05 20:04:43 +0200

    Add files via upload

---
+++ Test/ConsoleApp1/ConsoleApp1/Reception.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Reception:Room
+    {
+        List<Room> Rooms = Room.RoomData();
+        private double discount = 0.2;
+        public double getDiscount(int id)
+        {
+            if (id == 2) return discount;
+            else return 0;
+        }
+        public int getPrice(int nights, int id)
+        {
+            return Rooms.ElementAt(id - 1).priceNight * nights;
+        }
+        public bool makeReservation(int id)
+        {
+            if (Rooms.ElementAt(id - 1).status == false) return true;
+            else return false;
+        }
+
+        public void  handRoom(int id)
+        {
+            Rooms.ElementAt(id - 1).status = true;
+        }
+    }
+}
