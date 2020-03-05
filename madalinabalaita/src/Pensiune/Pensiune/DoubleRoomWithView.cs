e198bfc3b2d02ad456b96d78e614c8668562a907
madalinabalaita <madalinabalaita14@gmail.com>
2020-03-05 19:47:53 +0200

    update

---
+++ madalinabalaita/src/Pensiune/Pensiune/DoubleRoomWithView.cs
@@ -0,0 +1,17 @@
+﻿namespace Pensiune
+{
+   
+        public class DoubleRoomWithView:Rooms
+        {
+        public DoubleRoomWithView( int nrNights) : base( nrNights)
+        {
+            
+        }
+        public override double get_price()
+        {
+            if (nrNights > 3) return 150 * 3 + 150 * (nrNights - 3) * 0.8;
+            return 150 * nrNights;
+        }
+    }
+    
+}
