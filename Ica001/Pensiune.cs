19a5f0076431c986a9116144dd7a5c7b234ea035
Ica001 <61554943+Ica001@users.noreply.github.com>
2020-03-05 20:13:09 +0200

    Add files via upload

---
+++ Ica001/Pensiune.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Pensiune
+    {
+        Room[] rooms = new Room[6];
+        public Pensiune(Room[] rooms)
+        {
+            this.rooms = rooms;
+        }
+        
+    }
+}
